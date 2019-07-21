27c116bbd7e3dadc1217252b7f356d914d9de20b
Yury Nudga <yury.nudga@icord.cz>
2019-07-22 00:55:20 +0200

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import 'bulma/css/bulma.min.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 
 import { App } from './App';
+
+Sentry.init({ dsn: "https://24b8ef37b8fd4a06ab2b0d8205814675@sentry.io/1329204" });
 
 ReactDOM.render(<App />, document.getElementById('root'));
